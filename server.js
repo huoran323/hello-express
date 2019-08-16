d25903fedbde010953b00dc9f7a3ea2a870d1c14
霍然 <303176152@qq.com>
2019-08-16 10:48:00 +0800

    路由参数

--- server.js
+++ server.js
@@ -7,7 +7,30 @@ var app = express();
 // 定义路由，第一个参数为路径，'/'代表根目录，function添加两个参数，请求和响应
 app.get("/", function(req, res) {
   // 把信息响应给浏览器
-  res.send("this is homepage");
+  // res.send("this is homepage");
+  // 发送json、数组都可以
+  var responseObject = [
+    {
+      name: "haha"
+    },
+    { name: "hehe" }
+  ];
+  res.send(responseObject);
+});
+
+// 路由参数
+app.get("/profile/:id/user/:name", function(req, res) {
+  // 获取路由参数的id
+  res.send(
+    "You requested to see a profile with the name of " +
+      req.params.id +
+      req.params.name
+  );
+});
+
+// ？代表前面的参数b可以出现一次或者0次(可以有，也可以没有)
+app.get("/ab?cd", function(req, res) {
+  res.send("/ab?cd");
 });
 
 // 添加到监听的端口上
